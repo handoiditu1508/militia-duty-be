aeafd58c46f774a49c592b6957e721d4d7d30c2a
Danh <handoiditu1508@gmail.com>
2024-01-29 19:13:30 +0700

    add Militias DutyDates tables

--- Data/MilitiaContext.cs
+++ Data/MilitiaContext.cs
@@ -1,8 +1,52 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MilitiaDuty.Models.DutyDates;
+using MilitiaDuty.Models.Militias;
 
 namespace MilitiaDuty.Data
 {
     public class MilitiaContext : DbContext
     {
+        public DbSet<Militia> Militias { get; set; }
+        public DbSet<DutyDate> DutyDates { get; set; }
+        public DbSet<MilitiaDutyDate> MilitiaDutyDates { get; set; }
+
+        public string DbPath { get; }
+
+        public MilitiaContext()
+        {
+            var folder = Environment.SpecialFolder.UserProfile;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = Path.Join(path, "militia.db");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlite($"Data Source={DbPath}");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // config many to many between DutyDate and Militia
+            modelBuilder.Entity<DutyDate>()
+                .HasMany(d => d.Militias)
+                .WithMany(m => m.DutyDates)
+                .UsingEntity<MilitiaDutyDate>(
+                    md => md
+                        .HasOne<Militia>()
+                        .WithMany()
+                        .HasForeignKey(md => md.MilitiaId),
+                    md => md
+                        .HasOne<DutyDate>()
+                        .WithMany()
+                        .HasForeignKey(md => md.DutyDateId),
+                    md =>
+                    {
+                        md.HasKey(md => new { md.MilitiaId, md.DutyDateId });
+                    }
+                );
+        }
     }
 }
---
+++ Migrations/20240129121019_Add-Militias-DutyDates.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MilitiaDuty.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMilitiasDutyDates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DutyDates",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "TEXT", nullable: false),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    IsFullDutyDate = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DutyDates", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Militias",
+                columns: table => new
+                {
+                    Id = table.Column<uint>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Score = table.Column<ushort>(type: "INTEGER", nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Militias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MilitiaDutyDates",
+                columns: table => new
+                {
+                    MilitiaId = table.Column<uint>(type: "INTEGER", nullable: false),
+                    DutyDateId = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MilitiaDutyDates", x => new { x.MilitiaId, x.DutyDateId });
+                    table.ForeignKey(
+                        name: "FK_MilitiaDutyDates_DutyDates_DutyDateId",
+                        column: x => x.DutyDateId,
+                        principalTable: "DutyDates",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MilitiaDutyDates_Militias_MilitiaId",
+                        column: x => x.MilitiaId,
+                        principalTable: "Militias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MilitiaDutyDates_DutyDateId",
+                table: "MilitiaDutyDates",
+                column: "DutyDateId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MilitiaDutyDates");
+
+            migrationBuilder.DropTable(
+                name: "DutyDates");
+
+            migrationBuilder.DropTable(
+                name: "Militias");
+        }
+    }
+}
