042f0459f1ea1050f6705fe54e53d6e052f345c4
Danh <handoiditu1508@gmail.com>
2024-02-08 12:41:05 +0700

    add OnDutyDate for rule type

---
+++ Migrations/20240208053752_Add_RuleOnDutyRate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MilitiaDuty.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_RuleOnDutyRate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<ushort>(
+                name: "OnDutyRate",
+                table: "Rules",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OnDutyRate",
+                table: "Rules");
+        }
+    }
+}
