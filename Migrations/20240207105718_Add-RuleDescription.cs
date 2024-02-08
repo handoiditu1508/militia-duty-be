ac2430eed6f64276ab2a0c0ac5632c3883fbfb5c
Danh <handoiditu1508@gmail.com>
2024-02-08 11:30:38 +0700

    add rule description

---
+++ Migrations/20240207105718_Add-RuleDescription.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MilitiaDuty.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRuleDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Rules",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Rules");
+        }
+    }
+}
