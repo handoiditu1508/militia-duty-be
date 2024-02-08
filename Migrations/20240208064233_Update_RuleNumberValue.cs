457431dc65f9c9018e48cf0e88ea6d24f65da717
Danh <handoiditu1508@gmail.com>
2024-02-08 13:43:05 +0700

    rename Rule NumberValue

---
+++ Migrations/20240208064233_Update_RuleNumberValue.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MilitiaDuty.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_RuleNumberValue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "OnDutyRate",
+                table: "Rules",
+                newName: "NumberValue");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NumberValue",
+                table: "Rules",
+                newName: "OnDutyRate");
+        }
+    }
+}
