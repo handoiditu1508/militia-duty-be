d068c3afd73bfa0c67d5cb0bfbf567ca3937c965
Danh <handoiditu1508@gmail.com>
2024-02-08 11:31:16 +0700

    add config options

---
+++ Models/Options/MilitiaOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace MilitiaDuty.Models.Options
+{
+    public class MilitiaOptions
+    {
+        public const string Militia = "Militia";
+
+        public ushort IdealMilitiaPerDate { get; set; }
+        public ushort OnDutyRate { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using MilitiaDuty.Data;
+using MilitiaDuty.Models.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<MilitiaContext>();
+
+builder.Services.Configure<MilitiaOptions>(builder.Configuration.GetRequiredSection(MilitiaOptions.Militia));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +14,13 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Seed database
+var options = app.Configuration.GetRequiredSection(MilitiaOptions.Militia).Get<MilitiaOptions>();
+if (options != null)
+{
+    await Seeder.Seed(options);
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
